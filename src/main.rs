df58dc8b741f58a7bb78819ed7424f9b26cc060b
yujgewt <compaso@naver.com>
2020-11-30 20:22:03 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+/*
+Seed generator
+     - get Target File
+     - recognize file formats...
+
+*/
+use std::env;
+use std::fs;
+
+struct InputFile<'a>{
+    name : &'a str,
+    format : &'a str,
+
+}
+
+
+
+fn main(){
+    
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}",args);
+
+    let file_name = &args[1];
+    println!("file_name : {:?}",file_name);
+
+
+    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file.");
+    println!("contents : {:?}",contents);
+
+
+
 }
