1e5eb253c5e404b9eac444b0f9f43cd1e2ecd629
yujgewt <compaso@naver.com>
2020-11-30 20:35:50 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ Seed generator
 use std::env;
 use std::fs;
 
+enum Format{
+    EXE,
+    BIN
+}
+
+
 struct InputFile<'a>{
     name : &'a str,
-    format : &'a str,
+    format : Format,
 
 }
 
@@ -20,13 +26,20 @@ fn main(){
     let args: Vec<String> = env::args().collect();
     println!("{:?}",args);
 
+
+
+
     let file_name = &args[1];
     println!("file_name : {:?}",file_name);
 
-
     let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file.");
     println!("contents : {:?}",contents);
+    
 
+
+    let input = InputFile {name:file_name,format:Format::EXE};
+
+    
 
 
 }
