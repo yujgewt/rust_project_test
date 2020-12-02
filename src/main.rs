718b53a7556cd9da0cdc4ed7a0173d6d67a8e5b1
yujgewt <compaso@naver.com>
2020-12-02 18:09:29 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,17 @@ use std::env;
 use std::fs;
 use std::path::Path;
 use std::process;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
+#[derive(Debug)]
 enum Format {
     EXE,
     BIN,
 }
 
+#[derive(Debug)]
 struct InputFile<'a> {
     name: &'a str,
     format: Format,
@@ -23,7 +28,7 @@ fn check_files(file_name: &str) -> bool {
     return Path::new(file_name).exists();
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
@@ -31,13 +36,26 @@ fn main() {
     let file_name = &args[1];
     println!("file_name : {:?}", file_name);
 
-    if !check_files(file_name){
-        process::exit(0);
-    }
+    println!("try to open the file.");
+    let mut file = File::open(file_name)?;
+    
+    println!("try to read the file.");
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    println!("contents : {:?}",contents);
+    let input = InputFile {name:file_name,format:Format::EXE};
+    println!("input : {:?}",input);
+    Ok(())
+
+    // if !check_files(file_name){
+    //     println!("File")
+    //     process::exit(0);
+    // }
 
 
     // let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file.");
-    // println!("contents : {:?}",contents);
+    
 
-    // let input = InputFile {name:file_name,format:Format::EXE};
+    
 }
